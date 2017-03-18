fcfc8157284cdcba087c68af584bdffab90824ba
denial <denialwins6@gmail.com>
2017-03-18 16:06:48 +0200

    denial_homework_7

---
+++ Assets/Script_rigibody.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_rigibody : MonoBehaviour {
+    public float speed;
+    private Rigidbody _rigibody;
+	// Use this for initialization
+	void Start () {
+        _rigibody = GetComponent<Rigidbody>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical);
+        _rigibody.AddForce(direction * speed);
+	}
+}
